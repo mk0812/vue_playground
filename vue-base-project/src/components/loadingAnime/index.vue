355f5b579c0453d9fff566f31f610dd8b14aa787
mk_limitbreak <matsuoka812930@gmail.com>
2019-03-17 00:16:46 +0900

    修正

---
+++ vue-base-project/src/components/loadingAnime/index.vue
@@ -0,0 +1,35 @@
+<script>
+import axios from 'axios'
+import Loading from '@/components/loadingAnime/loading.vue'
+export default {
+    components: {
+        Loading
+    },
+    data() {
+        return {
+            height: 0,
+            list : []
+        }
+    },
+    watch: {
+        list: () => {
+            this.$nextTick(() => {
+                this.height = this.$refs.body.getBoundingClientRect().height
+            })
+        }
+    },
+    methods: {
+        loadContent() {
+            this.list = []
+            axios.get('/data/data.json').then(response => {
+                setTimeout(() => {
+                    this.list = response.data
+                },1500)
+            })
+        }
+    },
+    created() {
+        this.loadContent()
+    },
+}
+</script>
