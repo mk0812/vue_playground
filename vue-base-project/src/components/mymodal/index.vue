d9af4d654e1aceb13d1b255205bb4a2149225f20
mk_limitbreak <matsuoka812930@gmail.com>
2019-03-16 20:14:25 +0900

    modalwindow component define

---
+++ vue-base-project/src/components/mymodal/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="example-modal-window">
+        <p>modalWindow open</p>
+        <button @click="openModal">open</button>
+
+        <!-- ModalWindow Component define -->
+        <MyModal></MyModal>
+    </div>
+</template>
+
+<script>
+import MyModal from 'MyModal.vue'
+export default {
+    components: {
+        MyModal
+    },
+    data() {
+        return {
+            modal: false,
+            message: ''
+        }
+    },
+    methods: {
+        openModal: () => {
+            this.modal = true
+        },
+        closeModal: () => {
+            this.modal = false
+        },
+    },
+}
+</script>
